8d500598a61a1b29d2b1f416ebf72bf40231609c
Bradley S <bradleysantiago4@gmail.com>
2025-01-21 01:55:32 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css'
+
+
+export default App
+
