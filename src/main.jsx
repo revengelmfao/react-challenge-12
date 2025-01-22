098aa44d96d75e76e317fafb7afef997f9c99024
Bradley S <bradleysantiago4@gmail.com>
2025-01-22 00:58:12 -0500

    test branch first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App.jsx';
-import Resume from './components/pages/resume.jsx'; // Import your Resume component
+import Resume from '/components/pages/resume.jsx'; // Import your Resume component
 
 const root = createRoot(document.getElementById('root'));
 root.render(
